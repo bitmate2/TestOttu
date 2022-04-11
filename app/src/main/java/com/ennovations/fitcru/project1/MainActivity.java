83cb8520bb89565239fc28bb5c423ac49a48188b
Bit Mate <bitmate2@gmail.com>
2022-04-11 18:54:49 +0530

    branch1 commit intial

---
+++ app/src/main/java/com/ennovations/fitcru/project1/MainActivity.java
@@ -0,0 +1,30 @@
+package com.ennovations.fitcru.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.tv);
+
+        // code in branch1
+        tv.setText("branch1");
+        tv.setText("branch11");
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MainActivity2.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/ennovations/fitcru/project1/MainActivity2.java
@@ -0,0 +1,19 @@
+package com.ennovations.fitcru.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button btn = findViewById(R.id.btn);
+        Button btn1 = findViewById(R.id.btn);
+
+    }
+}
